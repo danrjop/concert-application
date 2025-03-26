a962cd96ac0c21f9a3c5914aeb606cf8feb36ccc
danrjop <dan-wang@live.com>
2025-03-26 02:20:30 -0700

    navi

---
+++ concert-application/concert-application/concert_applicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  concert_applicationApp.swift
+//  concert-application
+//
+//  Created by Daniel Wang on 3/26/25.
+//
+
+import SwiftUI
+
+@main
+struct concert_applicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ concert-application/concert-applicationTests/concert_applicationTests.swift
@@ -0,0 +1,17 @@
+//
+//  concert_applicationTests.swift
+//  concert-applicationTests
+//
+//  Created by Daniel Wang on 3/26/25.
+//
+
+import Testing
+@testable import concert_application
+
+struct concert_applicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
